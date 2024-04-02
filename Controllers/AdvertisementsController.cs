d2d8aa6985ebe35d2203bf9421372e1566f15869
Oleksandr Bazurin <a.bazurin@gmail.com>
2024-04-02 06:48:32 -0600

    update get ads to be annonymus

--- Controllers/AdvertisementsController.cs
+++ Controllers/AdvertisementsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NOLA_API.Application.Advertisements;
 using NOLA_API.DataModels;
@@ -7,6 +8,7 @@ namespace NOLA_API.Controllers
     public class AdvertisementsController : BaseApiController
     {
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> GetAdvertisements()
         {
             return HandleResult(await Mediator.Send(new Show.Query()));
