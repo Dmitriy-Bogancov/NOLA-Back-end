22bff8e630c9f67788564385c93077b919589eb0
Oleksandr Bazurin <a.bazurin@gmail.com>
2024-04-23 12:02:17 -0600

    update password verification

--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -8,7 +8,7 @@ namespace NOLA_API.DTOs
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,10}$", ErrorMessage = "Password must contain Uppercase char, Lowercase char, Special symbol and be 4-10 symbols long!")]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$", ErrorMessage = "Password must contain Uppercase char, Lowercase char, Special symbol and be 8-16 symbols long!")]
         public string Password { get; set; }
     }
 }
